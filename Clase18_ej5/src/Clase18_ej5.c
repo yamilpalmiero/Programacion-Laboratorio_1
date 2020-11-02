331249fbeab0d592cfb4fbfce02f163db2ff682a
Yamil Palmiero <yamilpalmiero@gmail.com>
2020-11-02 20:18:00 -0300

    Archivo de texto

---
+++ Clase18_ej5/src/Clase18_ej5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h> //para strlen()
+#include <stdlib.h> //para system("pause")
+
+#define ARCH	"./Texto.txt"
+
+/*LECTURA-ESCRITURA DE ARCHIVOS TEXTO*/
+/*w = lecto/escritura.  Si no existe->Crea. Si Existe->Pisa
+ r = lectura. Posición al comienzo.
+ a = Si no existe->Crea. Si Existe->Agrega al final
+ */
+int main(void) {
+	FILE *pArch;
+	char palabras[30];
+	system("pause");
+	printf("\nSe va a generar por primera vez el archivo\n");
+	if ((pArch = fopen(ARCH, "w")) == NULL) {
+		printf("El archivo no puede ser abierto");
+		exit(1);
+	}
+
+	printf("Ingrese un texto");
+	gets(palabras);
+	fflush(stdin);
+
+	fwrite(palabras, sizeof(char), strlen(palabras), pArch);
+
+	fclose(pArch);
+
+	return 0;
+}
