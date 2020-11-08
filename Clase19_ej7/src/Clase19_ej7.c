4d496d9610635215879dcb3501db0ae83ed49a64
Yamil Palmiero <yamilpalmiero@gmail.com>
2020-11-08 11:43:01 -0300

    Burbujeo de array dinamico de tipo estructura

---
+++ Clase19_ej7/src/Clase19_ej7.c
@@ -0,0 +1,19 @@
+/*
+ *
+ void empleadosOrdenar(eEmpleado *this, int tam) {
+ int i, j;
+ eEmpleado aux;
+
+ for (i = 0; i < tam - 1; i++) {
+ for (j = i + 1; j < tam; j++) {
+
+ if ((*(this + i)).sueldo > ((*(this + j)).sueldo)) {
+ aux = *(this + i);
+ *(this + i) = *(this + j);
+ *(this + j) = aux;
+ }
+ }
+
+ }
+ }
+ */
